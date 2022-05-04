a89e63b268c029be6fe440a82a2f61ceb1f80bbd
Kevin Cruz <kevin_mcp@outlook.com>
2022-05-03 22:49:49 -0400

    refactor(sidebar and src/pages/index): agregando nuevas rutas para el renderizado
    Aregle los nombres de las rutas que deben ir segun corresponde

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,100 +1,106 @@
 import { Flex, Text } from "@chakra-ui/react";
-import { RiFeedbackLine, RiHome4Line, RiMedal2Line, RiMessage2Line, RiUser3Line } from "react-icons/ri";
+import {
+  RiFeedbackLine,
+  RiHome4Line,
+  RiMedal2Line,
+  RiMessage2Line,
+  RiUser3Line,
+} from "react-icons/ri";
 import { Logo } from "../../assets/Logo";
 import { useColors } from "../../hooks/useColors";
 import { ActiveLink } from "./ActiveLink";
 
 export function Sidebar() {
-  const { colors } = useColors()
+  const { colors } = useColors();
 
   return (
     <Flex
-      position='sticky'
-      top='0'
-      h='100vh'
-      minW='250px'
-      maxW='250px'
+      position="sticky"
+      top="0"
+      h="100vh"
+      minW="250px"
+      maxW="250px"
       bg={colors.primary}
-      flexDir='column'
+      flexDir="column"
     >
       <Flex p={2}>
         <Logo />
       </Flex>
 
-      <ActiveLink href='/home'>
+      <ActiveLink href="/nutricionista/home">
         <Flex
           p={2}
-          cursor='pointer'
-          transition='0.2s filter'
+          cursor="pointer"
+          transition="0.2s filter"
           bg={colors.primary}
-          _hover={{ filter: 'brightness(120%)' }}
-          align='center'
+          _hover={{ filter: "brightness(120%)" }}
+          align="center"
           gap={2}
         >
-          <RiHome4Line size='25px' />
-          <Text fontSize='xl'>Home</Text>
+          <RiHome4Line size="25px" />
+          <Text fontSize="xl">Home</Text>
         </Flex>
       </ActiveLink>
 
-      <ActiveLink href='/clients'>
+      <ActiveLink href="/nutricionista/clients">
         <Flex
           p={2}
-          cursor='pointer'
-          transition='0.2s filter'
+          cursor="pointer"
+          transition="0.2s filter"
           bg={colors.primary}
-          _hover={{ filter: 'brightness(120%)' }}
-          align='center'
+          _hover={{ filter: "brightness(120%)" }}
+          align="center"
           gap={2}
         >
-          <RiUser3Line size='25px' />
-          <Text fontSize='xl'>Clientes</Text>
+          <RiUser3Line size="25px" />
+          <Text fontSize="xl">Clientes</Text>
         </Flex>
       </ActiveLink>
 
-      <ActiveLink href='#'>
+      <ActiveLink href="#">
         <Flex
           p={2}
-          cursor='pointer'
-          transition='0.2s filter'
+          cursor="pointer"
+          transition="0.2s filter"
           bg={colors.primary}
-          _hover={{ filter: 'brightness(120%)' }}
-          align='center'
+          _hover={{ filter: "brightness(120%)" }}
+          align="center"
           gap={2}
         >
-          <RiMessage2Line size='25px' />
-          <Text fontSize='xl'>Mensages</Text>
+          <RiMessage2Line size="25px" />
+          <Text fontSize="xl">Mensages</Text>
         </Flex>
       </ActiveLink>
 
-      <ActiveLink href='#'>
+      <ActiveLink href="#">
         <Flex
           p={2}
-          cursor='pointer'
-          transition='0.2s filter'
+          cursor="pointer"
+          transition="0.2s filter"
           bg={colors.primary}
-          _hover={{ filter: 'brightness(120%)' }}
-          align='center'
+          _hover={{ filter: "brightness(120%)" }}
+          align="center"
           gap={2}
         >
-          <RiMedal2Line size='25px' />
-          <Text fontSize='xl'>Agenda</Text>
+          <RiMedal2Line size="25px" />
+          <Text fontSize="xl">Agenda</Text>
         </Flex>
       </ActiveLink>
 
-      <ActiveLink href='#'>
+      <ActiveLink href="#">
         <Flex
           p={2}
-          cursor='pointer'
-          transition='0.2s filter'
+          cursor="pointer"
+          transition="0.2s filter"
           bg={colors.primary}
-          _hover={{ filter: 'brightness(120%)' }}
-          align='center'
+          _hover={{ filter: "brightness(120%)" }}
+          align="center"
           gap={2}
         >
-          <RiFeedbackLine size='25px' />
-          <Text fontSize='xl'>Feed</Text>
+          <RiFeedbackLine size="25px" />
+          <Text fontSize="xl">Feed</Text>
         </Flex>
       </ActiveLink>
     </Flex>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
 
     console.log(data);
 
-    router.push("/home");
+    router.push("/nutricionista/home");
   };
 
   return (
