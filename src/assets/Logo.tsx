250addf416052fc6630c476f40b037fc2cf73f0b
Kevin Cruz <kevin_mcp@outlook.com>
2022-05-17 23:33:28 -0400

    refactor(logo): cambio en el uso de Imagen
    Cambio al importar y utilizar imagen de logo

--- src/assets/Logo.tsx
+++ src/assets/Logo.tsx
@@ -1,4 +1,6 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import Image from "next/image";
+import LogoImg from "../../public/DuxVitaeLogo.png";
 
 // type Logo = {
 //   logo: HTMLImageElement;
@@ -8,7 +10,7 @@ import { Flex, Image } from "@chakra-ui/react";
 export function Logo() {
   return (
     <Flex>
-      <Image boxSize={130} src="DuxVitaeLogo.png" alt="Logo Dux Vitae" />
+      <Image width={180} height={180} src={LogoImg} alt="Logo Dux Vitae" />
     </Flex>
   );
 }
