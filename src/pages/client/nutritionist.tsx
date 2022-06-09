d3dea5d3c62d5b5b37cac9168e0fc2f7fa4d48b5
Kevin Cruz <kevin_mcp@outlook.com>
2022-06-09 02:49:02 -0400

    refactor(pages/client/nutritionist.tsx): llamando al backend el listado de nutricionista
    se esta recuperando el listado de nutricionistas usando api, para recorrerla renderizando cada
    nutricionista con el tablecontentnutritionist component

--- src/pages/client/nutritionist.tsx
+++ src/pages/client/nutritionist.tsx
@@ -1,9 +1,24 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  Flex,
+  Input,
+  Table,
+  TableCaption,
+  TableContainer,
+  Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { TableContentNutritionist } from "../../components/TableContentNutritionist";
 import { useColors } from "../../hooks/useColors";
 import { setupAPIClient } from "../../services/api";
+import { api } from "../../services/apiClient";
 import { withSSRAuth } from "../../utils/withSSRAuth";
 
-export default function nutritionist() {
+type NutritionistsProps = [{ rut: string; name: string; lastName: string }];
+
+export default function client({ nutritionists }) {
   const { colors } = useColors();
 
   return (
@@ -15,11 +30,50 @@ export default function nutritionist() {
         "calc(100vw - 250px)",
       ]}
       h="calc(100vh - 60px)"
+      align="center"
+      justify="center"
       bg={colors.bg}
-      justifyContent={"center"}
-      alignItems={"center"}
     >
-      <Flex w={["100%", "90%", "60%"]} h="31rem" bg="white"></Flex>
+      <TableContainer w="80%">
+        <Text color={colors.color} mb="8px">
+          Cliente:
+        </Text>
+        <Input
+          // onChange={handleChange}
+          placeholder="Buscar"
+          size="sm"
+          w="30%"
+          minW="200px"
+        />
+        <Table w="100%" variant="striped">
+          <TableCaption>Tabla de clientes</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Rut</Th>
+              <Th>Nombre</Th>
+              <Th>Apellido</Th>
+              <Th></Th>
+            </Tr>
+          </Thead>
+          {nutritionists.map((nutritionists) => (
+            <TableContentNutritionist
+              key={nutritionists.rut}
+              rut={nutritionists.rut}
+              name={nutritionists.name}
+              lastName={nutritionists.lastName}
+            />
+          ))}
+
+          <Tfoot>
+            <Tr>
+              <Th>Rut</Th>
+              <Th>Nombre</Th>
+              <Th>Apellido</Th>
+              <Th></Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
     </Flex>
   );
 }
@@ -27,10 +81,14 @@ export default function nutritionist() {
 export const getServerSideProps = withSSRAuth(
   async (ctx) => {
     const apiClient = setupAPIClient(ctx);
-    const response = await apiClient.get("/me");
+    const client = await apiClient.get("/me");
 
+    const response = await api.get("/nutritionists");
+    let nutritionists = response.data;
     return {
-      props: {},
+      props: {
+        nutritionists,
+      },
     };
   },
   {
