d383fb453d2a743082e72428946023398d450a54
Kevin Cruz <kevin_mcp@outlook.com>
2022-06-26 13:16:41 -0400

    feat(componentents/tablecontentcertifica): agregue la tablas de certificados
    agregue la tablas de certificados en la pagina de certificados del admin

---
+++ src/components/TableContentCertificate/index.tsx
@@ -0,0 +1,178 @@
+import {
+  Avatar,
+  Button as ButtonChakra,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Td,
+  Text,
+  Tr,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { parseCookies } from "nookies";
+import { useEffect, useState } from "react";
+import { ArchivePDF } from "../../assets/ArchivePDF";
+import { useColors } from "../../hooks/useColors";
+import { downloadCertificate } from "../../pages/admin/certificate";
+import { Button } from "../Button";
+
+type NutritionistProps = {
+  rut: string;
+  email: string;
+  name: string;
+  lastName: string;
+  created_at: Date;
+};
+
+type TableContentProps = {
+  idCertificate: string;
+  file: string;
+  state: boolean;
+  created_at: Date;
+  adminRut: string;
+  nutritionist: NutritionistProps;
+};
+
+type AppointmentData = {
+  title: string;
+  description: string;
+};
+
+export function TableContentCertificate({
+  idCertificate,
+  file,
+  state,
+  created_at,
+  adminRut,
+  nutritionist,
+}: TableContentProps) {
+  const { colors } = useColors();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [run, setRun] = useState("");
+
+  const fileName = file.split("-");
+
+  useEffect(() => {
+    const cookies = parseCookies(undefined);
+    setRun(cookies["rut"]);
+  }, []);
+
+  return (
+    <>
+      <Tr color={colors.color}>
+        <Td>{nutritionist.rut}</Td>
+        <Td>{nutritionist.name} </Td>
+        <Td>{nutritionist.lastName}</Td>
+
+        <Td display="flex" alignItems={"center"} gap="1rem">
+          <ButtonChakra
+            bg="transparent"
+            type="button"
+            onClick={() => {
+              downloadCertificate(fileName[0], file);
+            }}
+          >
+            <ArchivePDF />
+          </ButtonChakra>
+          <Button
+            bg={colors.tertiary}
+            name="Perfil"
+            type="button"
+            onClick={() => {
+              onOpen();
+            }}
+          />
+        </Td>
+      </Tr>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent bg={colors.primary} color={"#FFFFFF"}>
+          <ModalHeader>
+            <Flex align={"center"} gap={3}>
+              <Avatar name={nutritionist.name + " " + nutritionist.lastName} />
+              Perfil de {nutritionist.name}
+            </Flex>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody gap={2}>
+            <Flex flexDir={"column"} flex="1" gap={3}>
+              <Flex
+                bg={"#85000F"}
+                borderRadius={"5"}
+                flexDir={"column"}
+                flex="1"
+                paddingLeft={3}
+              >
+                <Text fontSize={"1.1rem"} fontWeight={"bold"}>
+                  Rut:
+                </Text>
+                <Text>{nutritionist.rut}</Text>
+              </Flex>
+
+              <Flex
+                bg={"#85000F"}
+                borderRadius={"5"}
+                flexDir={"column"}
+                flex="1"
+                paddingLeft={3}
+              >
+                <Text fontSize={"1.1rem"} fontWeight={"bold"}>
+                  Nombre:
+                </Text>
+                <Text>{nutritionist.name + " " + nutritionist.lastName}</Text>
+              </Flex>
+
+              <Flex
+                bg={"#85000F"}
+                borderRadius={"5"}
+                flexDir={"column"}
+                flex="1"
+                paddingLeft={3}
+              >
+                <Text fontSize={"1.1rem"} fontWeight={"bold"}>
+                  Email:
+                </Text>
+                <Text>{nutritionist.email}</Text>
+              </Flex>
+
+              <Flex
+                bg={"#85000F"}
+                borderRadius={"5"}
+                flexDir={"column"}
+                flex="1"
+                paddingLeft={3}
+              >
+                <Text fontSize={"1.1rem"} fontWeight={"bold"}>
+                  Descripción
+                </Text>
+                <Text>Nada.</Text>
+              </Flex>
+            </Flex>
+          </ModalBody>
+
+          <ModalFooter gap={3}>
+            <Button
+              name="Cerrar"
+              border={"none"}
+              bg={colors.primary}
+              onClick={onClose}
+            />
+            <Button
+              name="Aceptar"
+              borderColor={"#a92a39"}
+              bg={colors.primary}
+              onClick={() => {}}
+            />
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
--- src/pages/admin/certificate.tsx
+++ src/pages/admin/certificate.tsx
@@ -1,47 +1,82 @@
 import {
   Flex,
-  Select,
+  Input,
   Table,
   TableCaption,
   TableContainer,
+  Tbody,
   Text,
   Tfoot,
   Th,
   Thead,
   Tr,
 } from "@chakra-ui/react";
-import { TableContent } from "../../components/TableContent";
+import fileDownload from "js-file-download";
+import { TableContentCertificate } from "../../components/TableContentCertificate";
 import { useColors } from "../../hooks/useColors";
 import { setupAPIClient } from "../../services/api";
+import { api } from "../../services/apiClient";
 import { withSSRAuth } from "../../utils/withSSRAuth";
 
-export default function certificate() {
+type NutritionistProps = {
+  rut: string;
+  email: string;
+  name: string;
+  lastName: string;
+  created_at: Date;
+};
+
+type ListCertificates = {
+  listCertificates: [
+    {
+      idCertificate: string;
+      file: string;
+      state: boolean;
+      created_at: Date;
+      adminRut: string;
+      nutritionistRut: string;
+      nutritionist: NutritionistProps;
+    }
+  ];
+};
+
+export function downloadCertificate(fileName: string, file: string) {
+  api({
+    url: `/certificate/${file}`,
+    method: "GET",
+    responseType: "blob",
+  }).then((res) => {
+    fileDownload(res.data, fileName);
+  });
+}
+
+export default function certificate({ listCertificates }: ListCertificates) {
   const { colors } = useColors();
 
   return (
     <Flex
+      flex="1"
       w={[
         "calc(100vw - 50px)",
         "calc(100vw - 50px)",
         "calc(100vw - 50px)",
         "calc(100vw - 250px)",
       ]}
-      h="calc(100vh - 60px)"
+      align="top"
+      justify="center"
       bg={colors.bg}
-      justifyContent={"center"}
-      alignItems={"center"}
     >
       <TableContainer w="80%">
         <Text color={colors.color} mb="8px">
-          Certificates:
+          Certificado:
         </Text>
-
-        <Select placeholder="Buscar" size="sm" w="30%" minW="200px">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </Select>
-
+        <Input
+          // onChange={handleChange}
+          placeholder="Buscar"
+          size="sm"
+          w="30%"
+          minW="200px"
+        />
         <Table w="100%" variant="striped">
           <TableCaption>Tabla de certificados</TableCaption>
           <Thead>
@@ -49,20 +84,29 @@ export default function certificate() {
               <Th>Rut</Th>
               <Th>Nombre</Th>
               <Th>Apellido</Th>
-              <Th>Estado</Th>
-              <Th></Th>
+              <Th>Certificado</Th>
             </Tr>
           </Thead>
 
-          <TableContent />
-
+          <Tbody color={colors.color}>
+            {listCertificates.map((certificate) => (
+              <TableContentCertificate
+                key={certificate.idCertificate}
+                idCertificate={certificate.idCertificate}
+                file={certificate.file}
+                state={certificate.state}
+                created_at={certificate.created_at}
+                adminRut={certificate.adminRut}
+                nutritionist={certificate.nutritionist}
+              />
+            ))}
+          </Tbody>
           <Tfoot>
             <Tr>
               <Th>Rut</Th>
               <Th>Nombre</Th>
               <Th>Apellido</Th>
-              <Th>Estado</Th>
-              <Th></Th>
+              <Th>Certificado</Th>
             </Tr>
           </Tfoot>
         </Table>
@@ -76,8 +120,16 @@ export const getServerSideProps = withSSRAuth(
     const apiClient = setupAPIClient(ctx);
     const response = await apiClient.get("/me");
 
+    const certificates = await apiClient.get("/certificate");
+
+    const listCertificates = certificates.data;
+
+    console.log(listCertificates);
+
     return {
-      props: {},
+      props: {
+        listCertificates,
+      },
     };
   },
   {
