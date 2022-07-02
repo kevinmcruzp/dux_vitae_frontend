1eb0e31ecd2168ae2348d51323f15fe78eeb836e
Kevin Cruz <kevin_mcp@outlook.com>
2022-07-02 12:54:42 -0400

    refactor(client/minute.tsx): creando la pagina de minuta
    creando la pagina de minuta, con funcionalidades para hacer download archivos

--- src/pages/client/minute.tsx
+++ src/pages/client/minute.tsx
@@ -1,10 +1,40 @@
-import { Flex } from "@chakra-ui/react";
+import { Divider, Flex, Text } from "@chakra-ui/react";
+import fileDownload from "js-file-download";
+import { parseCookies } from "nookies";
+import { RiArrowRightSLine } from "react-icons/ri";
+import { ArchivePDF } from "../../assets/ArchivePDF";
 import { useColors } from "../../hooks/useColors";
 import { setupAPIClient } from "../../services/api";
+import { api } from "../../services/apiClient";
 import { withSSRAuth } from "../../utils/withSSRAuth";
 
-export default function minute() {
+type FileListProps = {
+  fileList: [
+    {
+      idFile: string;
+      filename: string;
+      originalname: string;
+      created_at: Date;
+    }
+  ];
+};
+
+function downloadFile(originalname: string, file: string) {
+  api({
+    url: `/file/${file}`,
+    method: "GET",
+    responseType: "blob",
+  }).then((res) => {
+    fileDownload(res.data, originalname);
+  });
+}
+
+export default function minute({ fileList }: FileListProps) {
   const { colors } = useColors();
+
+  function convertDate(date: Date) {
+    return new Date(date).toLocaleString();
+  }
 
   return (
     <Flex
@@ -16,10 +46,54 @@ export default function minute() {
       ]}
       h="calc(100vh - 60px)"
       bg={colors.bg}
-      justifyContent={"center"}
-      alignItems={"center"}
+      color={colors.color}
     >
-      <Flex w={["100%", "90%", "60%"]} h="31rem" bg="white"></Flex>
+      <Flex
+        flex="1"
+        backgroundImage="linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)) , url('/bgFrutas.png')"
+        backgroundSize={[
+          "calc(100vw - 50px)",
+          "calc(100vw - 50px)",
+          "calc(100vw - 50px)",
+          "calc(100vw - 250px)",
+        ]}
+        align="center"
+        justifyContent="center"
+      >
+        <Flex
+          w={["100%", "90%", "60%"]}
+          h="31rem"
+          bg={colors.chat}
+          p={5}
+          gap={5}
+          flexDir="column"
+          borderRadius={5}
+        >
+          {/*tabla de certificado*/}
+          {fileList.map((file) => (
+            <span key={file.idFile}>
+              <Flex
+                align={"center"}
+                justifyContent="space-between"
+                _hover={{ cursor: "pointer" }}
+                onClick={() => {
+                  downloadFile(file.originalname, file.filename);
+                }}
+              >
+                <Flex align={"center"} gap={5}>
+                  <ArchivePDF />
+                  <Text fontSize={15}> {file.originalname}</Text>
+                </Flex>
+                <Flex align={"center"} gap={10}>
+                  <Text>{convertDate(file.created_at)}</Text>
+                  <RiArrowRightSLine size={30} />
+                </Flex>
+              </Flex>
+              <Divider />
+            </span>
+          ))}
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
@@ -29,8 +103,16 @@ export const getServerSideProps = withSSRAuth(
     const apiClient = setupAPIClient(ctx);
     const response = await apiClient.get("/me");
 
+    const cookies = parseCookies(ctx);
+    const rut = cookies["rut"];
+
+    const files = await api.get(`/files/${rut}`);
+
+    const fileList = files.data;
+    console.log(fileList);
+
     return {
-      props: {},
+      props: { fileList },
     };
   },
   {
